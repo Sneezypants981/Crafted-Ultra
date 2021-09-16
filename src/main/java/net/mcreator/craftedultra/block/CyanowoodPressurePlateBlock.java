93fdab5d489993ee0f92b2bb3efe044fab52194a
Sneezypants981 <TC@Gravulum>
2021-09-16 14:56:03 -0500

    I Re-did Cyanowood and Blue Forest, The Biome Has Warped Spores instead of Falling Obsidian Tears.

---
+++ src/main/java/net/mcreator/craftedultra/block/CyanowoodPressurePlateBlock.java
@@ -0,0 +1,58 @@
+
+package net.mcreator.craftedultra.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.PressurePlateBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.craftedultra.CraftedUltraModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@CraftedUltraModElements.ModElement.Tag
+public class CyanowoodPressurePlateBlock extends CraftedUltraModElements.ModElement {
+	@ObjectHolder("crafted_ultra:cyanowood_pressure_plate")
+	public static final Block block = null;
+	public CyanowoodPressurePlateBlock(CraftedUltraModElements instance) {
+		super(instance, 10);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.REDSTONE)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends PressurePlateBlock {
+		public CustomBlock() {
+			super(Sensitivity.EVERYTHING,
+					Block.Properties.create(Material.WOOD).sound(SoundType.WOOD).hardnessAndResistance(2f, 3f).setLightLevel(s -> 0));
+			setRegistryName("cyanowood_pressure_plate");
+		}
+
+		@Override
+		public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+			return 5;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
--- src/main/java/net/mcreator/craftedultra/world/biome/BlueForestBiome.java
+++ src/main/java/net/mcreator/craftedultra/world/biome/BlueForestBiome.java
@@ -8,16 +8,17 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.common.BiomeManager;
 
-import net.minecraft.world.gen.trunkplacer.MegaJungleTrunkPlacer;
+import net.minecraft.world.gen.trunkplacer.StraightTrunkPlacer;
 import net.minecraft.world.gen.treedecorator.TrunkVineTreeDecorator;
 import net.minecraft.world.gen.treedecorator.TreeDecoratorType;
 import net.minecraft.world.gen.treedecorator.LeaveVineTreeDecorator;
+import net.minecraft.world.gen.treedecorator.CocoaTreeDecorator;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
 import net.minecraft.world.gen.placement.Placement;
 import net.minecraft.world.gen.placement.NoiseDependant;
 import net.minecraft.world.gen.placement.AtSurfaceWithExtraConfig;
-import net.minecraft.world.gen.foliageplacer.JungleFoliagePlacer;
+import net.minecraft.world.gen.foliageplacer.BlobFoliagePlacer;
 import net.minecraft.world.gen.feature.TwoLayerFeature;
 import net.minecraft.world.gen.feature.Features;
 import net.minecraft.world.gen.feature.FeatureSpread;
@@ -32,22 +33,27 @@ import net.minecraft.world.biome.BiomeGenerationSettings;
 import net.minecraft.world.biome.BiomeAmbience;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.IWorldWriter;
+import net.minecraft.world.ISeedReader;
 import net.minecraft.util.registry.WorldGenRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.math.MutableBoundingBox;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.RegistryKey;
+import net.minecraft.util.Direction;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
 
-import net.mcreator.craftedultra.block.Cyanowood_Wood_PackLogBlock;
-import net.mcreator.craftedultra.block.Cyanowood_Wood_PackLeavesBlock;
+import net.mcreator.craftedultra.block.CyanowoodLogBlock;
+import net.mcreator.craftedultra.block.CyanowoodLeavesBlock;
 import net.mcreator.craftedultra.CraftedUltraModElements;
 
 import java.util.Set;
+import java.util.Random;
+import java.util.List;
 
 import com.google.common.collect.ImmutableList;
 
@@ -62,22 +68,22 @@ public class BlueForestBiome extends CraftedUltraModElements.ModElement {
 		@SubscribeEvent
 		public void registerBiomes(RegistryEvent.Register<Biome> event) {
 			if (biome == null) {
-				BiomeAmbience effects = new BiomeAmbience.Builder().setFogColor(12638463).setWaterColor(-981001495).setWaterFogColor(-987561893)
-						.withSkyColor(7972607).withFoliageColor(10387789).withGrassColor(-987480891)
-						.setParticle(new ParticleEffectAmbience(ParticleTypes.DRIPPING_OBSIDIAN_TEAR, 0.005f)).build();
+				BiomeAmbience effects = new BiomeAmbience.Builder().setFogColor(12638463).setWaterColor(-11058795).setWaterFogColor(-16745537)
+						.withSkyColor(7972607).withFoliageColor(10387789).withGrassColor(-16756280)
+						.setParticle(new ParticleEffectAmbience(ParticleTypes.WARPED_SPORE, 0.005f)).build();
 				BiomeGenerationSettings.Builder biomeGenerationSettings = new BiomeGenerationSettings.Builder()
 						.withSurfaceBuilder(SurfaceBuilder.DEFAULT.func_242929_a(new SurfaceBuilderConfig(Blocks.GRASS_BLOCK.getDefaultState(),
 								Blocks.DIRT.getDefaultState(), Blocks.DIRT.getDefaultState())));
 				biomeGenerationSettings.withFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.TREE
-						.withConfiguration(
-								(new BaseTreeFeatureConfig.Builder(new SimpleBlockStateProvider(Cyanowood_Wood_PackLogBlock.block.getDefaultState()),
-										new SimpleBlockStateProvider(Cyanowood_Wood_PackLeavesBlock.block.getDefaultState()),
-										new JungleFoliagePlacer(FeatureSpread.func_242252_a(2), FeatureSpread.func_242252_a(0), 2),
-										new MegaJungleTrunkPlacer(7, 2, 19), new TwoLayerFeature(1, 1, 2))).setDecorators(
-												ImmutableList.of(CustomLeaveVineTreeDecorator.instance, CustomTrunkVineTreeDecorator.instance))
-												.setMaxWaterDepth(0).build())
+						.withConfiguration((new BaseTreeFeatureConfig.Builder(new SimpleBlockStateProvider(CyanowoodLogBlock.block.getDefaultState()),
+								new SimpleBlockStateProvider(CyanowoodLeavesBlock.block.getDefaultState()),
+								new BlobFoliagePlacer(FeatureSpread.func_242252_a(2), FeatureSpread.func_242252_a(0), 3),
+								new StraightTrunkPlacer(7, 2, 0), new TwoLayerFeature(1, 0, 1)))
+										.setDecorators(ImmutableList.of(CustomLeaveVineTreeDecorator.instance, CustomTrunkVineTreeDecorator.instance,
+												new CustomCocoaTreeDecorator()))
+										.setMaxWaterDepth(0).build())
 						.withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT)
-						.withPlacement(Placement.COUNT_EXTRA.configure(new AtSurfaceWithExtraConfig(1, 0.1F, 1))));
+						.withPlacement(Placement.COUNT_EXTRA.configure(new AtSurfaceWithExtraConfig(7, 0.1F, 1))));
 				biomeGenerationSettings.withFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
 						Feature.RANDOM_PATCH.withConfiguration(Features.Configs.GRASS_PATCH_CONFIG).withPlacement(Features.Placements.PATCH_PLACEMENT)
 								.withPlacement(Placement.COUNT_NOISE.configure(new NoiseDependant(-0.8D, 5, 4))));
@@ -88,11 +94,9 @@ public class BlueForestBiome extends CraftedUltraModElements.ModElement {
 				DefaultBiomeFeatures.withCavesAndCanyons(biomeGenerationSettings);
 				DefaultBiomeFeatures.withOverworldOres(biomeGenerationSettings);
 				DefaultBiomeFeatures.withFrozenTopLayer(biomeGenerationSettings);
-				DefaultBiomeFeatures.withFossils(biomeGenerationSettings);
 				MobSpawnInfo.Builder mobSpawnInfo = new MobSpawnInfo.Builder().isValidSpawnBiomeForPlayer();
-				mobSpawnInfo.withSpawner(EntityClassification.CREATURE, new MobSpawnInfo.Spawners(EntityType.RABBIT, 20, 4, 7));
-				mobSpawnInfo.withSpawner(EntityClassification.CREATURE, new MobSpawnInfo.Spawners(EntityType.IRON_GOLEM, 2, 1, 1));
-				mobSpawnInfo.withSpawner(EntityClassification.MONSTER, new MobSpawnInfo.Spawners(EntityType.CAVE_SPIDER, 20, 2, 3));
+				mobSpawnInfo.withSpawner(EntityClassification.MONSTER, new MobSpawnInfo.Spawners(EntityType.CAVE_SPIDER, 20, 2, 6));
+				mobSpawnInfo.withSpawner(EntityClassification.MONSTER, new MobSpawnInfo.Spawners(EntityType.VEX, 5, 1, 1));
 				biome = new Biome.Builder().precipitation(Biome.RainType.RAIN).category(Biome.Category.FOREST).depth(0.1f).scale(0.2f)
 						.temperature(0.5f).downfall(0.5f).setEffects(effects).withMobSpawnSettings(mobSpawnInfo.copy())
 						.withGenerationSettings(biomeGenerationSettings.build()).build();
@@ -144,6 +148,48 @@ public class BlueForestBiome extends CraftedUltraModElements.ModElement {
 		@Override
 		protected void func_227424_a_(IWorldWriter ww, BlockPos bp, BooleanProperty bpr, Set<BlockPos> sbc, MutableBoundingBox mbb) {
 			this.func_227423_a_(ww, bp, Blocks.AIR.getDefaultState(), sbc, mbb);
+		}
+	}
+
+	private static class CustomCocoaTreeDecorator extends CocoaTreeDecorator {
+		public static final CustomCocoaTreeDecorator instance = new CustomCocoaTreeDecorator();
+		public static com.mojang.serialization.Codec<CustomCocoaTreeDecorator> codec;
+		public static TreeDecoratorType tdt;
+		static {
+			codec = com.mojang.serialization.Codec.unit(() -> instance);
+			tdt = new TreeDecoratorType(codec);
+			tdt.setRegistryName("blue_forest_ctd");
+			ForgeRegistries.TREE_DECORATOR_TYPES.register(tdt);
+		}
+		public CustomCocoaTreeDecorator() {
+			super(0.2f);
+		}
+
+		@Override
+		protected TreeDecoratorType<?> func_230380_a_() {
+			return tdt;
+		}
+
+		@Override
+		public void func_225576_a_(ISeedReader p_225576_1_, Random p_225576_2_, List<BlockPos> p_225576_3_, List<BlockPos> p_225576_4_,
+				Set<BlockPos> p_225576_5_, MutableBoundingBox p_225576_6_) {
+			if (!(p_225576_2_.nextFloat() >= 0.2F)) {
+				int i = p_225576_3_.get(0).getY();
+				p_225576_3_.stream().filter((p_236867_1_) -> {
+					return p_236867_1_.getY() - i <= 2;
+				}).forEach((p_242865_5_) -> {
+					for (Direction direction : Direction.Plane.HORIZONTAL) {
+						if (p_225576_2_.nextFloat() <= 0.25F) {
+							Direction direction1 = direction.getOpposite();
+							BlockPos blockpos = p_242865_5_.add(direction1.getXOffset(), 0, direction1.getZOffset());
+							if (Feature.isAirAt(p_225576_1_, blockpos)) {
+								BlockState blockstate = Blocks.AIR.getDefaultState();
+								this.func_227423_a_(p_225576_1_, blockpos, blockstate, p_225576_5_, p_225576_6_);
+							}
+						}
+					}
+				});
+			}
 		}
 	}
 }
